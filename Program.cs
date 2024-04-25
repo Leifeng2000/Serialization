aa840d7313308cd3786e8f4139e3405a9a4177e0
Leifeng2000 <leifeng7112@gmail.com>
2024-04-25 21:26:49 +0700

    u

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Nguyen van Tu";
+        StoreLink(name);
+        Console.WriteLine(ReadString());
+    }
+    static void StoreLink(string linkvalue)
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/"+ "Link.txt";
+        FileStream fs = new FileStream(path, FileMode.Create);
+        StreamWriter streamWriter = new StreamWriter(fs,System.Text.Encoding.UTF8);
+        streamWriter.Write(linkvalue);
+        streamWriter.Flush();
+        fs.Close();
+
+    }
+    static string ReadString()
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/"+ "Link.txt";
+        FileStream fs = new FileStream(path, FileMode.Open);
+        StreamReader streamReader = new StreamReader(fs,System.Text.Encoding.UTF8);
+        string giatri = streamReader.ReadToEnd();
+        streamReader.Close();
+        return giatri;
+    }
+}
